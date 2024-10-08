c179095dbf7cb1fee13265875013229bcfdf1a6c
vyshnaviar <vyshnaviar830@gmail.com>
2024-10-08 09:51:40 -0700

    Added Higher_lower game

---
+++ Day13-Higher lower game/game_data.py
@@ -0,0 +1,38 @@
+data = [
+    {
+        'name' : 'Instagram',
+        'follower_count' : 346,
+        'description' : 'Social media platform',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Christiano Ronaldo',
+        'follower_count' : 215,
+        'description' : 'Footballer',
+        'country' : 'Portugal'
+    },
+    {
+        'name' : 'Ariana Grande',
+        'follower_count' : 183,
+        'description' : 'Music and actress',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Johnson',
+        'follower_count' : 181,
+        'description' : 'Actor',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Selena Gomez',
+        'follower_count' : 174,
+        'description' : 'Music and actress',
+        'country' : 'United States'
+    },
+
+    
+    
+    
+    
+
+]
